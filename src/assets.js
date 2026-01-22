0ccdfab2fc6c62af3b193bb82723d6fa49669b52
Oluwashina <shinzbaba@gmail.com>
2026-01-22 02:25:46 +0100

    update: portfolio assets

--- src/assets.js
+++ src/assets.js
@@ -1,3 +1,7 @@
-export const portfolioAssets = {
-  "assets": []
-};
+export const portfolioAssets = [
+  {
+    "id": "1769044873171",
+    "url": "data:image/jpeg;base64,",
+    "category": "portrait"
+  }
+];
