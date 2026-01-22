cd865317fc7cc93d1d5a94fcd32e6baa86788e3c
Oluwashina <shinzbaba@gmail.com>
2026-01-22 02:09:12 +0100

    update: portfolio assets

--- src/assets.js
+++ src/assets.js
@@ -1,10 +1,5 @@
 export const portfolioAssets = [
   {
-    "id": "e1",
-    "url": "/event_1.png",
-    "category": "event"
-  },
-  {
     "id": "e2",
     "url": "/event_2.png",
     "category": "event"
